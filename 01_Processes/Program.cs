51252e569070d7ca5c8dbc5e4aacf5545f61c032
Dasha <dashakonopelko@gmail.com>
2024-03-25 20:11:45 +0200

    Add project files.

---
+++ 01_Processes/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.Diagnostics;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        /*Process current = Process.GetCurrentProcess();
+        current.PriorityClass = ProcessPriorityClass.High;
+        Console.WriteLine($"---------------------- Current Processes Info -------------------------------");
+        Console.WriteLine($"PriorityClass            :: {current.PriorityClass}");
+        Console.WriteLine($"ProcessName              :: {current.ProcessName}");
+        Console.WriteLine($"Id                       :: {current.Id}");
+        Console.WriteLine($"MachineName              :: {current.MachineName}");
+        Console.WriteLine($"PrivateMemorySize (Kb)   :: {current.PrivateMemorySize64 / 1024}");
+        Console.WriteLine($"StartTime                :: {current.StartTime}");
+        Console.WriteLine($"TotalProcessorTime       :: {current.TotalProcessorTime}");
+        Console.WriteLine($"UserProcessorTime        :: {current.UserProcessorTime}");*/
+
+        // All processes
+
+        /*Process[] processes = Process.GetProcesses();
+        foreach (Process pr in processes) {
+            try {
+                Console.WriteLine($"{pr.ProcessName,-50} {pr.Id,-5} {pr.PriorityClass,-10} {pr.StartTime}");
+            }
+            catch(Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Error with {pr.ProcessName}");
+                Console.ResetColor();
+            }
+
+        
+        }*/
+
+        // Start Process
+
+        /*var pr = Process.Start("Calc.exe");
+        Console.WriteLine($"{pr.ProcessName,-50} {pr.Id,-5} {pr.PriorityClass,-10} {pr.StartTime}");*/
+
+        //Process.Start("C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe", "google.com stackoverflow.com");
+
+        ProcessStartInfo info = new ProcessStartInfo()
+        {
+            FileName = "notepad",
+            Arguments = $@"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}\888.txt",
+            WindowStyle = ProcessWindowStyle.Maximized
+        };
+        Process pr = Process.Start(info);
+        Console.ReadKey();
+
+        //pr.Close();
+        //pr.CloseMainWindow(); // alt + F4
+        pr.Kill();
+        Console.WriteLine("Operation has done!");
+        Console.WriteLine("Wait for exit");
+        pr.WaitForExit();
+        Console.WriteLine("Process was exit ...");
+        Console.WriteLine($"Exit code :: {pr.ExitCode}");
+        Console.WriteLine($"Exit time :: {pr.ExitTime}");
+    }
+}
---
+++ 02_TaskManager/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System.Diagnostics;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace _02_TaskManager
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DispatcherTimer timer = null;
+        public MainWindow()
+        {
+            InitializeComponent();
+            timer = new DispatcherTimer();
+            timer.Interval = new TimeSpan(0, 0, 2);
+            timer.Tick += timer_Tick;
+            timer.Start();
+            
+        }
+
+        private void Refresh(object sender, RoutedEventArgs e)
+        {
+            grid.ItemsSource = Process.GetProcesses();
+            
+        }
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            //MessageBox.Show("Time");
+        }
+
+        private void Details(object sender, RoutedEventArgs e)
+        {
+            var res = grid.SelectedItem as Process;
+            MessageBox.Show(res.Id.ToString());
+            var det = Process.GetProcessById(res.Id);
+        }
+
+        private void Stop(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void grid_ContextMenuOpening(object sender, ContextMenuEventArgs e)
+        {
+            var res = grid.SelectedItem as Process;
+            MessageBox.Show(res.Id.ToString());
+        }
+
+        private void grid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var res = grid.SelectedItem as Process;
+            MessageBox.Show(res.Id.ToString());
+        }
+    }
+}
